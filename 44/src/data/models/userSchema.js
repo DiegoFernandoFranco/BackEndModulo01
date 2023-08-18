dc9387a3ffd50666b64ccde10a25059018790fd0
diego franco <ilgattoink@gmail.com>
2023-08-17 21:45:54 -0300

    Entrega clase 44

---
+++ 44/src/data/models/userSchema.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+import paginate from "mongoose-paginate-v2";
+
+const userCollection = 'users';
+
+const UserSchema = new Schema({
+    firstName: { type: Schema.Types.String, required: true },
+    lastName: { type: Schema.Types.String },
+    email: { type: Schema.Types.String, unique: true, required: true },
+    age: { type: Schema.Types.Number, default: 18 },
+    // role: { type: Schema.Types.ObjectId, index: true, ref: 'roles', default: 'client' },
+    isAdmin: {type: Schema.Types.Boolean, default: false},
+    password: { type: Schema.Types.String }
+});
+
+UserSchema.plugin(paginate);
+
+export default mongoose.model(userCollection, UserSchema);
